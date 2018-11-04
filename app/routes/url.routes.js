a0cde6fbdb7d95cbe01b802367903f16bae6ea46
SwathiSathish <swathimay21@gmail.com>
2018-11-04 16:07:34 +0530

    used js file system to persist the data

--- app/routes/url.routes.js
+++ app/routes/url.routes.js
@@ -1,4 +1,5 @@
 const shortner = require('../../shortner');
+const fs = require('fs')
 const get_all_urls = [];
 
 
@@ -8,19 +9,41 @@ module.exports = function(app, passport){
         let URL = shortner.expand(req.params.shortcode);
         res.redirect(URL);    
     });
-    
-    app.post('/shorten', function(req, res){
+
+    app.post('/shorten', function (req, res) {
         let url = req.body.url;
         let shortcode = shortner.shorten(url);
-        res.send(shortcode);       
-        // var fullUrl = req.protocol + '://' + req.get('host') + '/'+ shortcode;
+    
+        /* As we are not using any database, it is good to persist the data in a file and read it when necessary */
+        fs.appendFile("urls.txt", '\n'+ shortcode, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        }); 
         get_all_urls.push(shortcode);
-    });
+    })
+
+
+    
+    // app.post('/shorten', function(req, res){
+    //     let url = req.body.url;
+    //     let shortcode = shortner.shorten(url);
+    //     res.send(shortcode);       
+    //     // var fullUrl = req.protocol + '://' + req.get('host') + '/'+ shortcode;
+    //     get_all_urls.push(shortcode);
+    // });
 
     
     app.get("/url/all", function(req,res){
         console.log("Get All url");
-        return res.send(get_all_urls);
+        fs.readFile('urls.txt',  "utf8",  function(err, data) {
+            console.log(get_all_urls);
+            if(data){
+                return res.send(data.split('\n').filter(v=>v!=''));
+            }
+      });
+        // return res.send(get_all_urls);
     });
 
     app.delete("/delete/url", function(req,res){
