e848960598267dea17b1234d85a45ed264885d8f
SwathiSathish <swathimay21@gmail.com>
2018-11-06 12:57:48 +0530

    used in-memory to store the urls in an array

--- app/routes/url.routes.js
+++ app/routes/url.routes.js
@@ -1,6 +1,6 @@
 const shortner = require('../../shortner');
 const fs = require('fs')
-// const get_all_urls = [];
+const get_all_urls = [];
 
 
 module.exports = function(app, passport){  
@@ -15,50 +15,62 @@ module.exports = function(app, passport){
         let shortcode = shortner.shorten(url);
     
         /* As we are not using any database, it is good to persist the data in a file and read it when necessary */
-        fs.appendFile("urls.txt", '\n'+ shortcode, function(err) {
-            if(err) {
-                return console.log(err);
-            }
-            console.log("The file was saved!");
-        }); 
-        // get_all_urls.push(shortcode);
+        // fs.appendFile("urls.txt", '\n'+ shortcode, function(err) {
+        //     if(err) {
+        //         return console.log(err);
+        //     }
+        //     console.log("The file was saved!");
+        // }); 
+        get_all_urls.push(shortcode);
     })
 
     
     app.get("/url/all", function(req,res){
         console.log("Get All url");
-        fs.readFile('urls.txt',  "utf8",  function(err, data) {
-            // console.log(get_all_urls);
-            if(data){
-                return res.send(data.split('\n').filter(v=>v!=''));
-            }
-        });
-        // return res.send(get_all_urls);
+        // fs.readFile('urls.txt',  "utf8",  function(err, data) {
+        //     // console.log(get_all_urls);
+        //     if(data){
+        //         return res.send(data.split('\n').filter(v=>v!=''));
+        //     }
+        // });
+        return res.send(get_all_urls);
     });
 
     app.delete("/delete/:url", function(req,res){
 
-        fs.readFile('urls.txt',  "utf8",  function(err, data) {
-            if(data){
-                array = data.split('\n').filter(v=>v!='');
-                console.log(data, 'kkk');
-                var search_term = req.body.url;
+        // fs.readFile('urls.txt',  "utf8",  function(err, data) {
+        //     if(data){
+        //         array = data.split('\n').filter(v=>v!='');
+        //         console.log(data, 'kkk');
+        //         var search_term = req.body.url;
                 
-                for (var i = 0; i < array.length; i++) {
-                    if (array[i] === search_term) {
-                        array.splice(i, 1);
+        //         for (var i = 0; i < array.length; i++) {
+        //             if (array[i] === search_term) {
+        //                 array.splice(i, 1);
                     
-                        fs.writeFile('urls.txt',  array.join("\n"), function(){
-                            console.log('done');
-                        });
-                        // array.join('\n');
-                    }
-                }
+        //                 fs.writeFile('urls.txt',  array.join("\n"), function(){
+        //                     console.log('done');
+        //                 });
+        //                 // array.join('\n');
+        //             }
+        //         }
                 
-                console.log(array, 'hi');
-                return res.send(array);
+        //         console.log(array, 'hi');
+        //         return res.send(array);
+        //     }
+        // });
+
+        console.log("deleting a url");
+        var fullUrl = req.protocol + '://' + req.get('host') + '/'+ get_all_urls[0];
+        console.log(get_all_urls);
+        var array = get_all_urls; 
+        var search_term = req.body.url;
+         for (var i=array.length-1; i>=0; i--) {
+            if (array[i] === search_term) {
+                array.splice(i, 1);
             }
-        });
+        }
+        return res.send(array);
     });
 };  
 
